4b44390f4f2e38253d143874e9f6fdc6f97dea1a
Roman Kowert <roman.kowert@gmail.com>
2023-02-12 00:55:52 +0100

    categories 0.3

--- src/App.js
+++ src/App.js
@@ -40,18 +40,7 @@ const App = () => {
       <Banner />
       <div className="categories-container">
         {categories.map(({ id, title, imageUrl }) => (
-          <div key={id} className="category-container">
-            <div
-              className="background-image"
-              style={{
-                backgroundImage: `linear-gradient(rgba(0, 0, 0, .4), rgba(0, 0, 0, .4)), url(${imageUrl})`,
-              }}
-            />
-            <div className="category-body-container">
-              <h2>{title}</h2>
-              <p>Shop Now</p>
-            </div>
-          </div>
+
         ))}
       </div>
     </div>
