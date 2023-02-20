1c82e77b1757d2cc0fca40f51af23610dfbf960c
Roman Kowert <rkowert@posteo.de>
2023-02-20 21:31:54 +0100

    video problem

--- src/components/banner/banner.component.jsx
+++ src/components/banner/banner.component.jsx
@@ -1,9 +1,9 @@
 import "./banner.styles.scss";
-import video from "../../assets/videos/video.mp4";
+import video from "../../assets/videos/video1.mp4";
 
 const Banner = () => (
   <div className="banner">
-    <video src={video} muted></video>
+    <video src={video} autoPlay loop muted></video>
   </div>
 );
 
