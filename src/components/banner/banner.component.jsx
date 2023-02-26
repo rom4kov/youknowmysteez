425a98866163f61d535464c695f35963f127dc12
Roman Kowert <rkowert@posteo.de>
2023-02-26 20:02:22 +0100

    1. commit auf t14 linux

--- src/components/banner/banner.component.jsx
+++ src/components/banner/banner.component.jsx
@@ -3,12 +3,7 @@ import video from "../../assets/videos/video.mp4";
 
 const Banner = () => (
   <div className="banner">
-    <video src={video} muted></video>
-    <div className="video-overlay">
-      <p>ANMELDEN</p>
-      <h1 className="title">youknowmysteez</h1>
-      <p>WARENKORB</p>
-    </div>
+    <video src={video} autoPlay loop muted></video>
   </div>
 );
 
