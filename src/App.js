117e14cf1640b6dbc58935b268e74650dc38014c
Roman Kowert <rkowert@posteo.de>
2023-03-01 00:32:34 +0100

    started working on sign-in page

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import { Routes, Route } from "react-router-dom";
 import Navigation from "./routes/navigation/navigation.component";
 import Home from "./routes/home/home.component";
 import Shop from "./routes/shop/shop.component";
+import SignIn from "./routes/sign-in/sign-in.component";
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Navigation />}>
         <Route index element={<Home />} />
-        <Route path="/shop" element={<Shop />} />
+        <Route path="shop" element={<Shop />} />
+        <Route path="login" element={<SignIn />} />
       </Route>
     </Routes>
   );
---
+++ src/routes/sign-in/sign-in.component.jsx
@@ -0,0 +1,11 @@
+import "./sign-in.styles.scss";
+
+const SignIn = () => {
+  return (
+    <div>
+      <h2>Sign In Page</h2>
+    </div>
+  );
+};
+
+export default SignIn;
