e1a6492eaac7490aeda5b7330e10a55ac3241d60
Roman Kowert <rkowert@posteo.de>
2023-03-02 04:03:47 +0100

    sign in page

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,9 +1,15 @@
+import { signInWithGooglePopup } from "../../utils/firebase/firebase.utils";
 import "./sign-in.styles.scss";
 
 const SignIn = () => {
+  const logGoogleUser = async () => {
+    const response = await signInWithGooglePopup();
+    console.log(response);
+  };
   return (
-    <div>
+    <div className="sign-in">
       <h2>Sign In Page</h2>
+      <button onClick={logGoogleUser}>Anmelden mit Google Popup</button>
     </div>
   );
 };
--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -23,4 +23,10 @@ const firebaseConfig = {
 const firebaseApp = initializeApp(firebaseConfig);
 
 const provider = new GoogleAuthProvider();
-provider.setCustomParameters({});
+
+provider.setCustomParameters({
+  prompt: "select_account",
+});
+
+export const auth = getAuth();
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
