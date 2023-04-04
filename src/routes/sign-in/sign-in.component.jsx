c340f2cecc423a7e5989350f22404229a0b1152f
Roman Kowert <rkowert@proton.me>
2023-04-04 19:42:34 +0200

    working on sign-in form

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,27 +1,71 @@
+import { useState } from "react";
+
 import {
   signInWithGooglePopup,
   createUserDocumentFromAuth,
 } from "../../utils/firebase/firebase.utils";
 
+import FormInput from "../../components/form-input/form-input.component";
+import Button from "../../components/button/button.component";
 import SignUpForm from "../../components/sign-up-form/sign-up-form.component";
 
 import "./sign-in.styles.scss";
 
+const defaultFormFields = {
+  email: "",
+  password: "",
+};
+
 const SignIn = () => {
+  const [formFields, setFormFields] = useState(defaultFormFields);
+  const { email, password } = formFields;
+
+  const resetFormFields = () => {
+    setFormFields(defaultFormFields);
+  };
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormFields({ ...formFields, [name]: value });
+  };
+
   const logGoogleUser = async () => {
     const { user } = await signInWithGooglePopup();
     console.log(user);
     const userDocRef = await createUserDocumentFromAuth(user);
+    resetFormFields();
   };
 
   return (
     <div className="sign-in">
       <div className="login">
         <h2>Anmelden</h2>
-        <button onClick={logGoogleUser}>Anmelden mit Google Popup</button>
-        <button onClick={signInWithGooglePopup}>
-          Anmelden mit Google Redirect
-        </button>
+        <span>Melde dich per Email und Passwort an</span>
+        <form className="form-inputs">
+          <FormInput
+            label="Email"
+            type="email"
+            name="email"
+            value={email}
+            onChange={handleChange}
+            required
+          />
+
+          <FormInput
+            label="Passwort"
+            type="password"
+            name="password"
+            pattern=".{8,}"
+            value={password}
+            onChange={handleChange}
+            required
+          />
+
+          <Button type="submit">Anmelden</Button>
+          <Button onClick={logGoogleUser} buttonType="google">
+            Anmelden mit Google
+          </Button>
+        </form>
       </div>
       <SignUpForm className="sign-up" />
     </div>
