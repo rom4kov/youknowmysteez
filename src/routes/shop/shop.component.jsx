185376ce54014b177fd646fd9a6194b7e0b0592f
Roman Kowert <rkowert@proton.me>
2023-04-07 04:19:58 +0200

    working on 'setting up product context challenge'

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,9 +1,20 @@
+import SHOP_DATA from "../../shop-data.json";
+
 import "./shop.styles.scss";
 
 const Shop = () => {
   return (
     <div className="shop">
       <h1>THIS IS THE YOUKNOWMYSTEEZ SHOP</h1>
+      <div className="shop-items">
+        {SHOP_DATA.map(({ id, name, imageUrl, price }) => (
+          <div className="shop-item" key={id}>
+            <h2>{name}</h2>
+            <img src={imageUrl}></img>
+            <p>€ {price}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
