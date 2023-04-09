50a510f659d41c32923ee8b556065fe7aa672830
Roman Kowert <rkowert@proton.me>
2023-04-09 23:53:30 +0200

    challenge: make checkout route / comp 0.4

--- src/routes/checkout/checkout.component.jsx
+++ src/routes/checkout/checkout.component.jsx
@@ -1,12 +1,19 @@
+import { useContext } from "react";
+
+import { CartContext } from "../../contexts/cart.context";
+
 import "./checkout.styles.scss";
 
 import React from "react";
 
 const Checkout = () => {
+  const { cartItems } = useContext(CartContext);
+
   return (
     <div className="checkout-container">
       <div className="cart">
         <h2>Warenkorb</h2>
+        {{cartItems.map()}}
       </div>
       <div className="pay">
         <h2>Gesamtsumme</h2>
