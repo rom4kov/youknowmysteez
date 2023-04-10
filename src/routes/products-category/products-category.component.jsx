c671b7c36479c9a8441cbdc080ed16eb2f6844a7
Roman Kowert <rkowert@proton.me>
2023-04-10 23:59:31 +0200

    make category pages

---
+++ src/routes/products-category/products-category.component.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ProductsCategory = () => {
+  return <h2>ProductsCategory</h2>;
+};
+
+export default ProductsCategory;
--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,5 +1,7 @@
 import { useContext, Fragment } from "react";
 
+import { useNavigate } from "react-router-dom";
+
 import { CategoriesContext } from "../../contexts/categories.context";
 
 import ProductCard from "../../components/product-card/product-card.component";
@@ -11,22 +13,31 @@ const Shop = () => {
 
   // setProducts(SHOP_DATA);
 
+  const navigate = useNavigate();
+
   return (
     <Fragment>
       <div className="shop">
         <h1>THIS IS THE YOUKNOWMYSTEEZ SHOP</h1>
-        {Object.keys(categoriesMap).map((title) => (
-          <Fragment key={title}>
-            <h2 className="category-title">{title}</h2>
-            <div className="products-container">
-              {categoriesMap[title].map((product) => {
-                if (categoriesMap[title].indexOf(product) < 4) {
-                  return <ProductCard product={product} key={product.id} />;
-                }
-              })}
-            </div>
-          </Fragment>
-        ))}
+        {Object.keys(categoriesMap).map((title) => {
+          const goToCategoryPageHandler = () => {
+            navigate(`/${title}`);
+          };
+          return (
+            <Fragment key={title}>
+              <h2 className="category-title" onClick={goToCategoryPageHandler}>
+                {title}
+              </h2>
+              <div className="products-container">
+                {categoriesMap[title].map((product) => {
+                  if (categoriesMap[title].indexOf(product) < 4) {
+                    return <ProductCard product={product} key={product.id} />;
+                  }
+                })}
+              </div>
+            </Fragment>
+          );
+        })}
       </div>
     </Fragment>
   );
