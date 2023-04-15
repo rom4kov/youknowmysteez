14d61efb531dfcd4f977e865df8ead67c24b00bf
Roman Kowert <rkowert@proton.me>
2023-04-16 00:22:58 +0200

    challenge of migr cart.context to reducer (done)

--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -1,4 +1,4 @@
-import { createContext, useEffect, useState } from "react";
+import { createContext, useEffect, useReducer } from "react";
 
 const addCartItem = (cartItems, productToAdd) => {
   // find if cartItems contains productToAdd
@@ -46,11 +46,81 @@ export const CartContext = createContext({
   increaseQtyOfCartItem: () => {},
 });
 
+export const CART_ACTION_TYPES = {
+  SET_IS_CART_OPEN: "SET_IS_CART_OPEN",
+  SET_CART_ITEMS: "SET_CART_ITEMS",
+  SET_ITEM_COUNT: "SET_ITEM_COUNT",
+  SET_SUM_TOTAL: "SET_SUM_TOTAL",
+};
+
+const cartReducer = (state, action) => {
+  const { type, payload } = action;
+
+  switch (type) {
+    case CART_ACTION_TYPES.SET_IS_CART_OPEN:
+      return {
+        ...state,
+        isCartOpen: payload,
+      };
+    case CART_ACTION_TYPES.SET_CART_ITEMS:
+      return {
+        ...state,
+        cartItems: payload,
+      };
+    case CART_ACTION_TYPES.SET_ITEM_COUNT:
+      return {
+        ...state,
+        itemCount: payload,
+      };
+    case CART_ACTION_TYPES.SET_SUM_TOTAL:
+      return {
+        ...state,
+        sumTotal: payload,
+      };
+    default:
+      throw new Error(`Unhandled type ${type} in the userReducer`);
+  }
+};
+
+const INITIAL_STATE = {
+  isCartOpen: false,
+  cartItems: [],
+  itemCount: 0,
+  sumTotal: 0,
+};
+
 export const CartProvider = ({ children }) => {
-  const [isCartOpen, setIsCartOpen] = useState(false);
-  const [cartItems, setCartItems] = useState([]);
-  const [itemCount, setItemCount] = useState(0);
-  const [sumTotal, setSumTotal] = useState(0);
+  // const [isCartOpen, setIsCartOpen] = useState(false);
+  // const [cartItems, setCartItems] = useState([]);
+  // const [itemCount, setItemCount] = useState(0);
+  // const [sumTotal, setSumTotal] = useState(0);
+
+  const [{ isCartOpen, cartItems, itemCount, sumTotal }, dispatch] = useReducer(
+    cartReducer,
+    INITIAL_STATE
+  );
+
+  const setIsCartOpen = () => {
+    dispatch({
+      type: CART_ACTION_TYPES.SET_IS_CART_OPEN,
+      payload: !isCartOpen,
+    });
+  };
+
+  const setCartItems = (changeCartItems) => {
+    dispatch({
+      type: CART_ACTION_TYPES.SET_CART_ITEMS,
+      payload: changeCartItems,
+    });
+  };
+
+  const setItemCount = (newItemCount) => {
+    dispatch({ type: CART_ACTION_TYPES.SET_ITEM_COUNT, payload: newItemCount });
+  };
+
+  const setSumTotal = (newSumTotal) => {
+    dispatch({ type: CART_ACTION_TYPES.SET_SUM_TOTAL, payload: newSumTotal });
+  };
 
   useEffect(() => {
     const newItemCount = cartItems.reduce((a, b) => a + b.quantity, 0);
