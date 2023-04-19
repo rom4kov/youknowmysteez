5c35a6c5bfa1c1c6d25422bf35e7b6e9ec68f8e4
Roman Kowert <rkowert@proton.me>
2023-04-19 19:07:20 +0200

    164. Reselect Library (done) -> challenge 0.2

--- src/store/root-reducer.js
+++ src/store/root-reducer.js
@@ -2,8 +2,10 @@ import { combineReducers } from "redux";
 
 import { userReducer } from "./reducers/user.reducer";
 import { categoriesReducer } from "./reducers/category.reducer";
+import { cartReducer } from "./reducers/cart.reducer";
 
 export const rootReducer = combineReducers({
   user: userReducer,
   categories: categoriesReducer,
+  cart: cartReducer,
 });
