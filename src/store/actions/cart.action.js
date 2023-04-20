fc214c925eecf02079201f4367226acc8d9ae568
Roman Kowert <rkowert@proton.me>
2023-04-20 15:20:58 +0200

    164. chall 0.6 setCartIsOpen action lacked return

--- src/store/actions/cart.action.js
+++ src/store/actions/cart.action.js
@@ -4,12 +4,13 @@ import { CART_ACTION_TYPES } from "../redux-types/cart.types";
 
 export const setCartIsOpen = (isCartOpen) => {
   console.log("isCartOpen in cart.action: ", isCartOpen);
-  createAction(CART_ACTION_TYPES.SET_IS_CART_OPEN, isCartOpen);
+  return createAction(CART_ACTION_TYPES.SET_IS_CART_OPEN, isCartOpen);
 };
 
-export const setCartItems = ({ cartItems, itemCount, sumTotal }) =>
-  createAction(CART_ACTION_TYPES.SET_CART_ITEMS, {
+export const setCartItems = ({ cartItems, itemCount, sumTotal }) => {
+  return createAction(CART_ACTION_TYPES.SET_CART_ITEMS, {
     cartItems,
     itemCount,
     sumTotal,
   });
+};
