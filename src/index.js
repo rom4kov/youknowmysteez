fe5688d7369da1009558bcb3a731e6390d0e1fa8
QuentinAyers1917 <rkowert@posteo.de>
2023-10-02 23:22:47 +0200

    184. Using createSlice (done)

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-import { PersistGate } from "redux-persist/integration/react";
+// import { PersistGate } from "redux-persist/integration/react";
 
 import App from "./App";
 
@@ -17,11 +17,11 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
+      {/* <PersistGate loading={null} persistor={persistor}> */}
         <BrowserRouter>
           <App />
         </BrowserRouter>
-      </PersistGate>
+      {/* </PersistGate> */}
     </Provider>
   </React.StrictMode>
 );
--- src/store/store.js
+++ src/store/store.js
@@ -3,28 +3,29 @@ import {
   legacy_createStore as createStore,
   applyMiddleware,
 } from "redux";
-import { persistStore, persistReducer } from "redux-persist";
-import storage from "redux-persist/lib/storage";
+import { configureStore } from '@reduxjs/toolkit';
+// import { persistStore, persistReducer } from "redux-persist";
+// import storage from "redux-persist/lib/storage";
 
 import logger from "redux-logger";
 
-// import thunk from "redux-thunk";
+// // import thunk from "redux-thunk";
 
-import createSagaMiddleware from "redux-saga";
+// import createSagaMiddleware from "redux-saga";
 
-import { rootSaga } from "./root-saga";
+// import { rootSaga } from "./root-saga";
 
 import { rootReducer } from "./root-reducer";
 
-const persistConfig = {
-  key: "root",
-  storage,
-  whitelist: ["cart"],
-};
+// const persistConfig = {
+//   key: "root",
+//   storage,
+//   whitelist: ["cart"],
+// };
 
-const sagaMiddleware = createSagaMiddleware();
+// const sagaMiddleware = createSagaMiddleware();
 
-const persistedReducer = persistReducer(persistConfig, rootReducer);
+// const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 const middleWares = [
   process.env.NODE_ENV !== "production" && logger,
@@ -32,22 +33,27 @@ const middleWares = [
   sagaMiddleware,
 ].filter(Boolean);
 
-const composedEnhancer =
-  (process.env.NODE_ENV !== "production" &&
-    window &&
-    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
-  compose;
+// const composedEnhancer =
+//   (process.env.NODE_ENV !== "production" &&
+//     window &&
+//     window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
+//   compose;
 
-const composedEnhancers = composedEnhancer(applyMiddleware(...middleWares));
+// const composedEnhancers = composedEnhancer(applyMiddleware(...middleWares));
 
-export const store = createStore(
-  persistedReducer,
-  undefined,
-  composedEnhancers
-);
+export const store = configureStore({
+  reducer: rootReducer,
+  middleware: middleWares
+})
 
-sagaMiddleware.run(rootSaga);
+// export const store = createStore(
+//   persistedReducer,
+//   undefined,
+//   composedEnhancers
+// );
 
-export const persistor = persistStore(store);
+// sagaMiddleware.run(rootSaga);
+
+// export const persistor = persistStore(store);
 
 
