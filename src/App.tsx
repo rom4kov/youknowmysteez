a2963fb05eed449fb3ce763960c0a6abc3bd186e
QuentinAyers1917 <rkowert@posteo.de>
2023-10-26 20:49:11 +0200

    243. Code Splitting, Dynamic Imports, Suspense & Lazy (done)

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,23 @@
-import { useEffect } from "react";
+import { useEffect, lazy, Suspense } from "react";
 import { Routes, Route } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { useInView } from "react-intersection-observer";
-import Navigation from "./routes/navigation/navigation.component";
-import Home from "./routes/home/home.component";
-import Authentication from "./routes/authentication/authentication.component";
-import Shop from "./routes/shop/shop.component";
-import Cart from "./routes/cart/cart.component";
-import Payment from "./routes/payment/payment.component";
-import Confirmation from "./routes/payment/confirmation.component";
+import Spinner from "./components/spinner/spinner.component";
 import { checkUserSession } from "./store/reducers/user.reducer";
+
+const Home = lazy(() => import("./routes/home/home.component"));
+const Navigation = lazy(
+  () => import("./routes/navigation/navigation.component")
+);
+const Authentication = lazy(
+  () => import("./routes/authentication/authentication.component")
+);
+const Shop = lazy(() => import("./routes/shop/shop.component"));
+const Cart = lazy(() => import("./routes/cart/cart.component"));
+const Payment = lazy(() => import("./routes/payment/payment.component"));
+const Confirmation = lazy(
+  () => import("./routes/payment/confirmation.component")
+);
 
 const App = () => {
   const dispatch = useDispatch();
@@ -25,16 +33,18 @@ const App = () => {
   console.log(intObsRef);
 
   return (
-    <Routes>
-      <Route path="/" element={<Navigation navColor={navColor} />}>
-        <Route index element={<Home intObsRef={intObsRef} />} />
-        <Route path="auth" element={<Authentication />} />
-        <Route path="shop/*" element={<Shop />} />
-        <Route path="cart" element={<Cart />} />
-        <Route path="payment" element={<Payment />} />
-        <Route path="confirmation" element={<Confirmation />} />
-      </Route>
-    </Routes>
+    <Suspense fallback={<Spinner />}>
+      <Routes>
+        <Route path="/" element={<Navigation navColor={navColor} />}>
+          <Route index element={<Home intObsRef={intObsRef} />} />
+          <Route path="auth" element={<Authentication />} />
+          <Route path="shop/*" element={<Shop />} />
+          <Route path="cart" element={<Cart />} />
+          <Route path="payment" element={<Payment />} />
+          <Route path="confirmation" element={<Confirmation />} />
+        </Route>
+      </Routes>
+    </Suspense>
   );
 };
 
