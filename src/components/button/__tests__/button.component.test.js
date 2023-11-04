90d3f668bb5166dc38a1890f50e464f61c8693dd
QuentinAyers1917 <rkowert@posteo.de>
2023-11-04 20:28:30 +0100

    303. Button Tests - Part 2 (done)

--- src/components/button/__tests__/button.component.test.js
+++ src/components/button/__tests__/button.component.test.js
@@ -1,6 +1,5 @@
 import { render, screen } from "@testing-library/react";
-import Button from "../button.component";
-import { BUTTON_TYPE_CLASSES } from "../button.component";
+import Button, { BUTTON_TYPE_CLASSES } from "../button.component";
 
 describe("button tests", () => {
   test("should render base button when nothing is passed", () => {
@@ -9,15 +8,21 @@ describe("button tests", () => {
     expect(buttonElement).toHaveStyle("background-color: rgb(25, 59, 25)");
   });
 
-  test("should render GoogleSignInButton when google is passed", () => {
+  test("should render google button when google btn type is passed", () => {
     render(<Button buttonType={BUTTON_TYPE_CLASSES.google} />);
-    const buttonElement = screen.getByRole("button");
-    expect(buttonElement).toHaveStyle("background-color: #4285f4");
+    const googleButtonElement = screen.getByRole("button");
+    expect(googleButtonElement).toHaveStyle("background-color: #4285f4");
   });
 
-  test("should render InvertedButton when inverted is passed", () => {
+  test("should render inverted btn when inverted btn type is passed", () => {
     render(<Button buttonType={BUTTON_TYPE_CLASSES.inverted} />);
+    const invertedButtonElement = screen.getByRole("button");
+    expect(invertedButtonElement).toHaveStyle("background-color: white");
+  });
+
+  test("should be disabled if isLoading is true", () => {
+    render(<Button isLoading={true} />);
     const buttonElement = screen.getByRole("button");
-    expect(buttonElement).toHaveStyle("background-color: white");
+    expect(buttonElement).toBeDisabled();
   });
 });
