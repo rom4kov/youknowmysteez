f93d7c13b9510aa1ac00324bc904ed5c37bf0464
QuentinAyers1917 <rkowert@posteo.de>
2023-11-04 19:53:04 +0100

    302. Button Tests - Part 1 (chall done)

--- src/components/button/__tests__/button.component.test.js
+++ src/components/button/__tests__/button.component.test.js
@@ -1,10 +1,23 @@
 import { render, screen } from "@testing-library/react";
 import Button from "../button.component";
+import { BUTTON_TYPE_CLASSES } from "../button.component";
 
 describe("button tests", () => {
   test("should render base button when nothing is passed", () => {
     render(<Button />);
     const buttonElement = screen.getByRole("button");
     expect(buttonElement).toHaveStyle("background-color: rgb(25, 59, 25)");
+  });
+
+  test("should render GoogleSignInButton when google is passed", () => {
+    render(<Button buttonType={BUTTON_TYPE_CLASSES.google} />);
+    const buttonElement = screen.getByRole("button");
+    expect(buttonElement).toHaveStyle("background-color: #4285f4");
+  });
+
+  test("should render InvertedButton when inverted is passed", () => {
+    render(<Button buttonType={BUTTON_TYPE_CLASSES.inverted} />);
+    const buttonElement = screen.getByRole("button");
+    expect(buttonElement).toHaveStyle("background-color: white");
   });
 });
