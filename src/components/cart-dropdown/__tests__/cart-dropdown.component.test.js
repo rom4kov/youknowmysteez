3afc5fa092535cd4de00d8c331d75b83423f4793
QuentinAyers1917 <rkowert@posteo.de>
2023-11-07 20:41:12 +0100

    tests for banner and cart-dropdown component are working

---
+++ src/components/cart-dropdown/__tests__/cart-dropdown.component.test.js
@@ -0,0 +1,46 @@
+import { screen } from "@testing-library/react";
+import { renderWithProviders } from "../../../utils/test/test.utils";
+import CartDropdown from "../cart-dropdown.component";
+
+describe("CartDropdown tests", () => {
+  test("it should show cart items if cartItems.length > 0", () => {
+    renderWithProviders(<CartDropdown />, {
+      preloadedState: {
+        cart: {
+          cartItems: [
+            {
+              id: 1,
+              name: "nice jacket",
+              brand: "the jacket brand",
+              price: 70,
+              quantity: 1,
+            },
+            {
+              id: 2,
+              name: "another nice jacket",
+              brand: "the jacket brand",
+              price: 75,
+              quantity: 1,
+            },
+          ],
+        },
+      },
+    });
+
+    const emptyCartText = screen.queryByText(/keine artikel im warenkorb/i);
+    expect(emptyCartText).toBeNull();
+  });
+
+  test("it should show no cart items if cartItems.length = 0", () => {
+    renderWithProviders(<CartDropdown />, {
+      preloadedState: {
+        cart: {
+          cartItems: [],
+        },
+      },
+    });
+
+    const emptyCartText = screen.getByText(/keine artikel im warenkorb/i);
+    expect(emptyCartText).toBeInTheDocument();
+  });
+});
