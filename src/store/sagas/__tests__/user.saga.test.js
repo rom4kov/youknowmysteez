d0afae3849f776ee9ca2b948cf0f3b66b0c3e9f7
QuentinAyers1917 <rkowert@posteo.de>
2023-11-15 17:00:15 +0100

    user.saga.test.js works with all testSaga tests

--- src/store/sagas/__tests__/user.saga.test.js
+++ src/store/sagas/__tests__/user.saga.test.js
@@ -13,6 +13,11 @@ import {
   onEmailSignIn,
   onSignOutStart,
   isUserAuthenticated,
+  signUp,
+  signInAfterSignUp,
+  signInWithGoogle,
+  signInWithEmail,
+  signOut,
 } from "../user.saga";
 
 describe("User Saga tests", () => {
@@ -35,6 +40,46 @@ describe("User Saga tests", () => {
     testSaga(onCheckUserSession)
       .next()
       .takeLatest(USER_ACTION_TYPES.CHECK_USER_SESSION, isUserAuthenticated)
+      .next()
+      .isDone();
+  });
+
+  test("onSignUpStart", () => {
+    testSaga(onSignUpStart)
+      .next()
+      .takeLatest(USER_ACTION_TYPES.SIGN_UP_START, signUp)
+      .next()
+      .isDone();
+  });
+
+  test("onSignUpSuccess", () => {
+    testSaga(onSignUpSuccess)
+      .next()
+      .takeLatest(USER_ACTION_TYPES.SIGN_UP_SUCCESS, signInAfterSignUp)
+      .next()
+      .isDone();
+  });
+
+  test("onGoogleSignIn", () => {
+    testSaga(onGoogleSignIn)
+      .next()
+      .takeLatest(USER_ACTION_TYPES.GOOGLE_SIGN_IN_START, signInWithGoogle)
+      .next()
+      .isDone();
+  });
+
+  test("onEmailSignIn", () => {
+    testSaga(onEmailSignIn)
+      .next()
+      .takeLatest(USER_ACTION_TYPES.EMAIL_SIGN_IN_START, signInWithEmail)
+      .next()
+      .isDone();
+  });
+
+  test("onSignOutStart", () => {
+    testSaga(onSignOutStart)
+      .next()
+      .takeLatest(USER_ACTION_TYPES.SIGN_OUT_START, signOut)
       .next()
       .isDone();
   });
