14eb1c8c5c4396ccbcfa11865e3e9514825e85eb
QuentinAyers1917 <rkowert@posteo.de>
2024-11-13 17:03:53 +0100

    change style of payment form

--- src/components/payment-form/payment-form.styles.tsx
+++ src/components/payment-form/payment-form.styles.tsx
@@ -6,7 +6,7 @@ export const PaymentFormContainer = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  justify-content: center;
+  justify-content: start;
 `;
 
 export const FormContainer = styled.form`
